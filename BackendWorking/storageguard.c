15c190b6b465be350e2573419e14777c8a299bf7
Mohammad Makhamreh <149593099+mohammadmakhamreh@users.noreply.github.com>
2024-01-12 19:05:26 +0200

    Create storageguard.c

---
+++ BackendWorking/storageguard.c
@@ -0,0 +1,31 @@
+#include "header.h"
+
+void checkingTheStorage(Product* storageArea);
+
+void main(int argc, char *argv[]){
+    // opening the storageArea
+    printf("------ Hey Im storage guard!! \n");
+    key_t key3 = ftok(".", 's');
+    int shm_id3 = shmget(key3, SHM_SIZE, IPC_CREAT | 0666);
+    Product* storageArea =  (Product *) shmat(shm_id3, NULL, 0);
+    checkingTheStorage(storageArea);
+
+}
+
+void checkingTheStorage(Product* storageArea){
+
+    while(1){
+        int emptyProducts =0;
+        for(int i =0 ; i < numberOfItems ; i++){
+            if(storageArea[i].available_number == 0){
+                emptyProducts++;
+            }
+        }
+        if(emptyProducts == numberOfItems){
+            printf("The Storage Area is out of Stock, Lets close and take holiday!! \n");
+            kill(0, SIGTERM);
+        } else{
+            emptyProducts =0;
+        }
+    }
+}
